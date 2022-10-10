b1b27499bf46f6a2473e7bebf3c57beea4a78ea6
TunaMadu <michael.lopez19@my.stjohns.edu>
2022-10-10 10:50:50 -0400

    Done with button logic
    javascript is weird man

--- functions.js
+++ functions.js
@@ -6,13 +6,17 @@ document.addEventListener("DOMContentLoaded", () => {
   let inputField = document.querySelector("#task-input");
   let tasks = document.querySelector(".tasks");
 
+  // creates arrays to store regular tasks and important tasks separately
+  let taskArray = [];
+  let importantTaskArray = [];
+
   form.addEventListener("submit", (event) => {
     event.preventDefault();
 
     if (inputField.value === "") {
       return;
     }
-
+    //creates a new task
     let newTaskText = document.createElement("input");
     newTaskText.classList.add("userInput");
     newTaskText.type = "text";
@@ -47,14 +51,25 @@ document.addEventListener("DOMContentLoaded", () => {
 
     tasks.appendChild(newTask);
     inputField.value = "";
+    //
 
-    // lets begin with the important
+    taskArray.push(newTask);
 
+    // button logic
+
+    //important button
     importantButton.addEventListener("click", () => {
       if (newTask.id === "important-task") {
+        // this will remove the task that is no longer important
+        for (let i = 0; i < importantTaskArray.length; i++) {
+          if (importantTaskArray[i] === newTask) {
+            importantTaskArray.splice(i, 1);
+          }
+        }
         newTask.id = "";
         importantButton.id = "";
       } else {
+        importantTaskArray.push(newTask);
         newTask.id = "important-task";
         importantButton.id = "important-button";
       }
@@ -62,12 +77,24 @@ document.addEventListener("DOMContentLoaded", () => {
 
     // remove button
 
+    // will remove the task in array
+    // since arrays in js are not fixed we can also check if the element at i is in the important task array as well,
+    // we will not get a out of bound but simply a undefined value!
     removeButton.addEventListener("click", () => {
+      for (let i = 0; i < taskArray.length; i++) {
+        let checkImpTaskLength = importantTaskArray.length >= 1;
+        if (checkImpTaskLength && importantTaskArray[i] === newTask) {
+          importantTaskArray.splice(i, 1);
+        }
+
+        if (taskArray[i] === newTask) {
+          taskArray.splice(i, 1);
+        }
+      }
       newTask.remove();
     });
 
     // edit button
-
     editButton.addEventListener("click", () => {
       if (editButton.innerText === "Edit") {
         editButton.innerText = "Save";
