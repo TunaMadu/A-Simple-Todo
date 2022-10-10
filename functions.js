02be7b580bf8eb3295be65bbf4bae86651d7e2b4
TunaMadu <michael.lopez19@my.stjohns.edu>
2022-10-09 21:24:42 -0400

    Added the inital logic of todo list
    we are able to add new elements on the page as well as make sure we dont
    refresh the page when the user submits the form!

--- functions.js
+++ functions.js
@@ -1,5 +1,51 @@
 document.addEventListener("DOMContentLoaded", () => {
   // we should select the content in the input and make a new task
 
-  document.addEventListener("submit", () => {});
+  let form = document.querySelector("#form-section");
+
+  let inputField = document.querySelector("#task-input");
+  let tasks = document.querySelector(".tasks");
+
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    if (inputField.value === "") {
+      return;
+    }
+
+    let newTaskText = document.createElement("input");
+    newTaskText.classList.add("userInput");
+    newTaskText.type = "text";
+    newTaskText.value = inputField.value;
+    newTaskText.setAttribute("readonly", "readonly");
+
+    let textContainer = document.createElement("div");
+    textContainer.classList.add("task-content");
+
+    textContainer.appendChild(newTaskText);
+
+    let importantButton = document.createElement("button");
+    importantButton.innerText = "Important";
+
+    let editButton = document.createElement("button");
+    editButton.innerText = "Edit";
+
+    let removeButton = document.createElement("button");
+    removeButton.innerText = "Remove";
+
+    let actions = document.createElement("div");
+    actions.classList.add("actions");
+
+    actions.appendChild(importantButton);
+    actions.appendChild(editButton);
+    actions.appendChild(removeButton);
+
+    let newTask = document.createElement("div");
+    newTask.classList.add("task");
+    newTask.appendChild(textContainer);
+    newTask.appendChild(actions);
+
+    tasks.appendChild(newTask);
+    inputField.value = "";
+  });
 });
