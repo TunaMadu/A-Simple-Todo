fa85a51c47d0be9ed9c22e09d58c98bb7d859859
TunaMadu <michael.lopez19@my.stjohns.edu>
2023-01-07 22:20:15 -0500

    refactored code logic the main logic works though still need to work on the button logic

--- docs/assets/js/functions.js
+++ docs/assets/js/functions.js
@@ -1,108 +1,107 @@
-document.addEventListener('DOMContentLoaded', () => {
-    // we should select the content in the input and make a new task
+/**
+ * Will run the main logic once everything is loaded on DOM
+ */
 
+document.addEventListener('DOMContentLoaded', app);
+
+/**
+ * Will create a new task and will set the input value given by its @param
+ * @param {text} input will be from the user
+ * @returns will string containg html to be appended
+ */
+
+const createNewtask = function (input) {
+    return `<div class="task" id="">
+  <div class="task-content">
+  <input type="text" value ="${input}" class="userInput" readonly />
+  </div>
+  <div class="actions">
+  <button class = "action-btn" id="">Important</button>
+  <button class = "action-btn" >Edit</button>
+  <button class = "action-btn" >Remove</button>
+  </div>
+  </div>`;
+};
+
+const disableImportant = function (parentElem, childElem) {
+    parentElem.id = '';
+    childElem.id = '';
+};
+
+const enableImportant = function (parentElem, childElem) {
+    parentElem.id = 'important-task';
+    childElem.id = 'important-button';
+};
+
+/**
+ * Main app logic.
+ */
+
+function app() {
     let form = document.querySelector('#form-section');
-
     let inputField = document.querySelector('#task-input');
     let tasks = document.querySelector('.tasks');
 
     // creates arrays to store regular tasks and important tasks separately
     let taskArray = [];
-    let importantTaskArray = [];
 
     form.addEventListener('submit', (event) => {
         event.preventDefault();
 
-        if (inputField.value === '') {
-            return;
-        }
-        //creates a new task
-        let newTaskText = document.createElement('input');
-        newTaskText.classList.add('userInput');
-        newTaskText.type = 'text';
-        newTaskText.value = inputField.value;
-        newTaskText.setAttribute('readonly', 'readonly');
+        if (inputField.value === '') return;
 
-        let textContainer = document.createElement('div');
-        textContainer.classList.add('task-content');
+        // creates new task
+        tasks.innerHTML += createNewtask(inputField.value);
+        taskArray.push(tasks.lastElementChild);
 
-        textContainer.appendChild(newTaskText);
-
-        let importantButton = document.createElement('button');
-        importantButton.innerText = 'Important';
-
-        let editButton = document.createElement('button');
-        editButton.innerText = 'Edit';
-
-        let removeButton = document.createElement('button');
-        removeButton.innerText = 'Remove';
-
-        let actions = document.createElement('div');
-        actions.classList.add('actions');
-
-        actions.appendChild(importantButton);
-        actions.appendChild(editButton);
-        actions.appendChild(removeButton);
-
-        let newTask = document.createElement('div');
-        newTask.classList.add('task');
-        newTask.appendChild(textContainer);
-        newTask.appendChild(actions);
-
-        tasks.appendChild(newTask);
+        //clear input field
         inputField.value = '';
-        //
-
-        taskArray.push(newTask);
 
         // button logic
+        const actionButtons = document.querySelector('.actions');
 
         //important button
-        importantButton.addEventListener('click', () => {
-            if (newTask.id === 'important-task') {
-                // this will remove the task that is no longer important
-                for (let i = 0; i < importantTaskArray.length; i++) {
-                    if (importantTaskArray[i] === newTask) {
-                        importantTaskArray.splice(i, 1);
-                    }
-                }
-                newTask.id = '';
-                importantButton.id = '';
-            } else {
-                importantTaskArray.push(newTask);
-                newTask.id = 'important-task';
-                importantButton.id = 'important-button';
-            }
-        });
+        //TODO the button logic is giving me some issues though i think it has to deal with even delagation and just dom concepts in general!
+        // actionButtons.addEventListener('click', (event) => {
 
-        // remove button
+        //     if (actionButtons.parentElement.id === 'important-task') {
+        //         disableImportant(
+        //             actionButtons.parentElement,
+        //             actionButtons.firstElementChild
+        //         );
+        //     } else {
+        //         enableImportant(
+        //             actionButtons.parentElement,
+        //             actionButtons.firstElementChild
+        //         );
+        //     }
+        // });
 
-        // will remove the task in array
-        // since arrays in js are not fixed we can also check if the element at i is in the important task array as well,
-        // we will not get a out of bound but simply a undefined value!
-        removeButton.addEventListener('click', () => {
-            for (let i = 0; i < taskArray.length; i++) {
-                let checkImpTaskLength = importantTaskArray.length >= 1;
-                if (checkImpTaskLength && importantTaskArray[i] === newTask) {
-                    importantTaskArray.splice(i, 1);
-                }
+        // // remove button
 
-                if (taskArray[i] === newTask) {
-                    taskArray.splice(i, 1);
-                }
-            }
-            newTask.remove();
-        });
+        // // will remove the task in array
+        // removeButton.addEventListener('click', () => {
+        //     for (let i = 0; i < taskArray.length; i++) {
+        //         let checkImpTaskLength = importantTaskArray.length >= 1;
+        //         if (checkImpTaskLength && importantTaskArray[i] === newTask) {
+        //             importantTaskArray.splice(i, 1);
+        //         }
 
-        // edit button
-        editButton.addEventListener('click', () => {
-            if (editButton.innerText === 'Edit') {
-                editButton.innerText = 'Save';
-                newTaskText.removeAttribute('readonly');
-            } else {
-                newTaskText.setAttribute('readonly', 'readonly');
-                editButton.innerText = 'Edit';
-            }
-        });
+        //         if (taskArray[i] === newTask) {
+        //             taskArray.splice(i, 1);
+        //         }
+        //     }
+        //     newTask.remove();
+        // });
+
+        // // edit button
+        // editButton.addEventListener('click', () => {
+        //     if (editButton.innerText === 'Edit') {
+        //         editButton.innerText = 'Save';
+        //         newTaskText.removeAttribute('readonly');
+        //     } else {
+        //         newTaskText.setAttribute('readonly', 'readonly');
+        //         editButton.innerText = 'Edit';
+        //     }
     });
-});
+}
